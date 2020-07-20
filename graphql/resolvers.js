efc576477cf03d45b268d6b6ce5bb6420e71a8ad
ShashankBiplav <Biplavshashank7@gmail.com>
2020-07-20 23:34:17 +0530

    Added Mutation and Resolver for Creating a User

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,22 @@
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user.js');
+
 module.exports ={
-    hello(){
-        return {
-            text: 'Hello from GraphQL',
-            views: 7997
-        };
+    createUser: async function({ userInput }, req){
+        // const email = args.userInput.email; //use when not using destructuring
+        const existingUser = await User.findOne({ email: userInput.email});
+        if (existingUser) {
+            const error = new Error('User Exists');
+            throw error;
+        }
+       const hashedPassword = await bcrypt.hash(userInput.password, 12);
+       const user = new User({
+           email: userInput.email,
+           name: userInput.name,
+           password: hashedPassword
+       });
+       const createdUser = await user.save();
+       return {...createdUser._doc, _id: createdUser._id.toString()};
     }
 };
